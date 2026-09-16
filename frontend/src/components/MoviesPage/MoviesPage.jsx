d4cee223fd760f373f85e82af2f2c95092d95856
MythicalPlayz <eyaddodo05@gmail.com>
2026-09-17 01:26:40 +0300

    redid router

--- frontend/src/components/MoviesPage/MoviesPage.jsx
+++ frontend/src/components/MoviesPage/MoviesPage.jsx
@@ -4,25 +4,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClapperboard, faTicket } from '@fortawesome/free-solid-svg-icons';
 import translations from '../../lang/main.json';
 import { useLanguage } from '../../contexts/LanguageContext';
-import { Link } from 'react-router-dom';
+import { Link, useOutlet } from 'react-router-dom';
 
 export default function MoviesPage() {
   const { lang } = useLanguage();
+  const outlet = useOutlet();
 
-  return (
+  return !outlet ? (
     <div className="mx-auto w-5/6 lg:w-3/4 grid grid-cols-1 md:grid-cols-2 gap-8 my-4 min-h-screen">
-      <Link to="/movies/now-showing" className="w-full h-75 rounded-3xl bg-tertiary p-16 relative flex justify-center items-center">
-        <h2 className="text-5xl font-bold uppercase mb-4">{translations.nowShowing[lang]}</h2>
-        <div className='absolute top-0 left-0 bottom-0 right-0 opacity-50 flex justify-center items-center'>
-          <FontAwesomeIcon icon={faTicket} className="text-9xl rotate-12 text-quaternary scale-200"></FontAwesomeIcon>
+      <Link to="/movies/now-showing" className="w-full h-75 rounded-3xl bg-tertiary p-16 relative flex justify-center items-center overflow-hidden">
+        <h2 className="text-5xl font-bold uppercase mb-4 relative z-10">
+          {translations.nowShowing[lang]}
+        </h2>
+        <div className="absolute inset-0 opacity-50 flex justify-center items-center pointer-events-none">
+          <FontAwesomeIcon icon={faTicket} className="text-9xl rotate-12 text-quaternary scale-200"/>
         </div>
       </Link>
-      <Link to="/movies/coming-soon" className="w-full h-75 rounded-3xl bg-tertiary p-16 relative flex justify-center items-center">
-        <h2 className="text-5xl font-bold uppercase mb-4">{translations.comingSoon[lang]}</h2>
-        <div className='absolute top-0 left-0 bottom-0 right-0 opacity-50 flex justify-center items-center'>
-          <FontAwesomeIcon icon={faClapperboard} className="text-9xl rotate-12 text-quaternary scale-200"></FontAwesomeIcon>
+
+      <Link to="/movies/coming-soon" className="w-full h-75 rounded-3xl bg-tertiary p-16 relative flex justify-center items-center overflow-hidden">
+        <h2 className="text-5xl font-bold uppercase mb-4 relative z-10">
+          {translations.comingSoon[lang]}
+        </h2>
+        <div className="absolute inset-0 opacity-50 flex justify-center items-center pointer-events-none">
+          <FontAwesomeIcon icon={faClapperboard} className="text-9xl rotate-12 text-quaternary scale-200"/>
         </div>
       </Link>
     </div>
+  ) : (
+    outlet
   );
 }
